85afdf2abd5c634a27c04f339b55839686283126
Michal <michal@gmail.com>
2017-08-20 11:20:28 +0200

    Skills page made & testing varying cards design ideas that crossed my mind

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,29 +1,22 @@
 import React, { Component } from 'react';
+import TypeWriter from 'react-typewriter';
 import './Skills.css';
 
 export default class Skills extends Component {
     render() {
         return(
             <section id="bg-skills">
-                <div className="section-content">
-                    <div className="container">
-                        <h1>What do I do ?</h1>
-                        <p className="lead">Many things.</p>
-
-                        <div className="row">
-                            <div className="col-sm-12">
-                                <div className="card-group">
-                                    <div className="card">
-                                        <div className="card-block">
-                                            <h4 className="card-title">Witam</h4>
-                                            <h6 className="card-subtitle">No witam witam</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                           
-                        </div>
+                <div className="cards">
+                    <div className="card" id="card-1">
+                        <TypeWriter typing={ 1 } fixed={false} minDelay={ 50 } maxDelay={ 300 }>
+                            <h1 className="card-title" id="frontend">F<br/>R<br/>O<br/>N<br/>T<br/>E<br/>N<br/>D<br/></h1>
+                        </TypeWriter>
                     </div>
+                    <div className="card" id="card-2">
+                        <TypeWriter typing={ 1 } fixed={false} minDelay={ 50 } maxDelay={ 300 }>
+                            <h1 className="card-title" id="gamedev">G<br/>A<br/>M<br/>E<br/>D<br/>E<br/>V<br/></h1>
+                        </TypeWriter>
+                    </div>  
                 </div>
             </section>    
         );
