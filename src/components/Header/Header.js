c490c394110cbf476c01bc46f0c05ccfa50442ae
Michal <michal@gmail.com>
2017-08-20 10:09:59 +0200

    Redesigned home, deleted skills page ( changed approach ), media queries work almost fine on every device

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,18 +7,18 @@ export default class Header extends Component {
     render() {
         return(
             <nav>
-                <Link to="home"><span className="fa fa-home"></span></Link>
+                <Link to="home"><span className="fa fa-home nav-icon"></span></Link>
                 <div className="brand">    
-                    <span className="type">   
+                    <span className="typewriter">   
                         <TypeWriter typing={ 1 } fixed={true} minDelay={ 50 } maxDelay={ 300 }>
-                            <strong><name className="name">MichalDev</name> I'm coding web and making games sometimes.</strong>
+                            <span id="nav-title"><name id="name">MichalDev </name><g2> I'm coding web and making games sometimes.</g2></span>
                         </TypeWriter>
                     </span>
                 </div>
                 <div className="icons">
-                        <a href="https://www.instagram.com/gamedevguy/"><span className="fa fa-instagram"></span></a>
-                        <a href="https://github.com"><span className="fa fa-github"></span></a>
-                        <a href="https://www.artstation.com/michaldev"><span className="fa fa-gamepad"></span></a>
+                        <a href="https://www.instagram.com/gamedevguy/"><span className="fa fa-instagram nav-icon"></span></a>
+                        <a href="https://github.com"><span className="fa fa-github nav-icon"></span></a>
+                        <a href="https://www.artstation.com/michaldev"><span className="fa fa-gamepad nav-icon"></span></a>
                 </div>
             </nav>
         );
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,22 +4,28 @@ import './Skills.css';
 export default class Skills extends Component {
     render() {
         return(
-            <section className="bg-skills" id="second-section">
-                <div className="skill-container">
-                    <div className="skill">
-                        <h4 className="skill-title">Javascript</h4>
-                        <div className="skill-logo js"></div>
-                    </div>
-                    <div className="skill">
-                        <h4 className="skill-title">Unity</h4>
-                        <div className="skill-logo unity"></div>
-                    </div>
-                    <div className="skill">
-                        <h4 className="skill-title">Blender</h4>
-                        <div className="skill-logo blender"></div>
+            <section id="bg-skills">
+                <div className="section-content">
+                    <div className="container">
+                        <h1>What do I do ?</h1>
+                        <p className="lead">Many things.</p>
+
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <div className="card-group">
+                                    <div className="card">
+                                        <div className="card-block">
+                                            <h4 className="card-title">Witam</h4>
+                                            <h6 className="card-subtitle">No witam witam</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </div>
                     </div>
                 </div>
-            </section>     
+            </section>    
         );
     }
 }
