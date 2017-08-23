21427cb954f51240ead107232e3b46eec8f249f8
Michal <michal@gmail.com>
2017-08-23 19:58:58 +0200

    Totally reconstructed frontend skills page.

--- src/components/SkillFrontend/SkillFrontend.js
+++ src/components/SkillFrontend/SkillFrontend.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Header from '../Header/Header';
 import './SkillFrontend.css';
 
 export default class SkillFrontend extends Component {
@@ -10,19 +9,79 @@ export default class SkillFrontend extends Component {
 
     render() {
         return(
-            <div className="container" id="frontend-container">
-                <div className="col-md-4" id="javascript-card">
-                    <div className="rounded-circle" id="javascript-image" width="250" height="250"></div>
-                    <h2>Javascript</h2>
-                    <p className="lead">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula
-                    ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                </div>
-                <div className="col-md-4">
+            <div className="SkillFrontend">
+                <section id="frontend-background">
+                    <div className="container" id="frontend-container">
+                        <div className="row">
+                            <div className="col-sm-12 col-lg-3 frontend-card" id="js-card">
+                                <div className="rounded-circle" id="js-image" width="250" height="250"></div>
+                                <h2>Javascript</h2>
+                                <p className="lead">Most fucked up language.</p>
+                                <a href="#js-section" role="button" className="btn btn-primary btn-lg" id="js-button">More</a>
+                            </div>
+                            <div className="col-sm-12 col-lg-3 frontend-card" id="reactjs-card">
+                                <div className="rounded-circle" id="reactjs-image" width="250" height="250"></div>
+                                <h2>Reactjs</h2>
+                                <p className="lead">Most fucked up framework.</p>
+                                <a href="" role="button" className="btn btn-primary btn-lg" id="reactjs-button">More</a>
+                            </div>
+                            <div className="col-sm-12 col-lg-3 frontend-card" id="bootstrap-card">
+                                <div className="rounded-circle" id="bootstrap-image" width="250" height="250"></div>
+                                <h2>Bootstrap</h2>
+                                <p className="lead">Most fucked up css framework.</p>
+                                <a href="" role="button" className="btn btn-primary btn-lg" id="bootstrap-button">More</a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
-                </div>
-                <div className="col-md-4">
+                <section id="js-section">
+                    <div className="container">
+                        <div className="row feature-content">
+                                <div className="col-md-6 col-sm-12">
+                                    <div id="js-header-image"></div>
+                                </div>
+                                <div className="col-md-5 col-sm-12 col-md-offset-1">
+                                    <h6>Wubba Lubba Dub Dub.</h6>
+                                    <p className="lead">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula
+                                ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                </div>                       
+                        </div>
+                    </div>
+                </section>
 
-                </div>
+                <section id="reactjs-section">
+                    <div className="container">
+                        <div className="row feature-content">
+                                <div className="col-md-6 col-sm-12">
+                                    <h6>Reactjs powered this page bitch !</h6>
+                                    <p className="lead">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula
+                                ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                </div>        
+                                <div className="col-md-5 col-sm-12 col-md-offset-1">
+                                    <div id="reactjs-header-image"></div>
+                                </div>               
+                        </div>
+                    </div>
+                </section>
+
+                <section id="bootstrap-section">
+                    <div className="container">
+                        <div className="row feature-content">
+                                <div className="col-md-6 col-sm-12">
+                                    <div id="bootstrap-header-image"></div>
+                                </div>   
+
+                                <div className="col-md-5 col-sm-12 col-md-offset-1">
+                                    <h6>Bootstrap used for styling this page !</h6>
+                                    <p className="lead">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula
+                                ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                                </div>                    
+                        </div>
+                    </div>
+                </section>
+
+
             </div>
         );
     }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -7,22 +7,27 @@ export default class Skills extends Component {
     render() {
         return(
             <section id="Skills">
-                <div className="cards">
-                    <Link to="frontend" style={{ textDecoration: 'none' }}>
-                        <div className="card" id="card-1">
-                            <TypeWriter typing={ 1 } fixed={false} minDelay={ 50 } maxDelay={ 200 }>
-                                <h1 className="card-title" id="frontend">F<br/>R<br/>O<br/>N<br/>T<br/>E<br/>N<br/>D<br/></h1>
-                            </TypeWriter>
-                        </div>
-                    </Link>
+                <div className="container">
+                    <div className="row">
+                        <div className="cards">
+                            
+                            <Link to="frontend" style={{ textDecoration: 'none' }}>
+                                <div className="col-sm-12 col-md-12 card" id="card-1">
+                                    <TypeWriter typing={ 1 } fixed={false} minDelay={ 50 } maxDelay={ 200 }>
+                                        <h1 className="card-title" id="frontend">F<br/>R<br/>O<br/>N<br/>T<br/>E<br/>N<br/>D<br/></h1>
+                                    </TypeWriter>
+                                </div>
+                            </Link>
 
-                    <Link to="gamedev" style={{ textDecoration: 'none' }}>
-                    <div className="card" id="card-2">
-                        <TypeWriter typing={ 1 } fixed={false} minDelay={ 50 } maxDelay={ 200 }>
-                            <h1 className="card-title" id="gamedev">G<br/>A<br/>M<br/>E<br/>D<br/>E<br/>V<br/></h1>
-                        </TypeWriter>
-                    </div>  
-                    </Link>
+                            <Link to="gamedev" style={{ textDecoration: 'none' }}>
+                                <div className="col-sm-12 col-md-12 card" id="card-2">
+                                    <TypeWriter typing={ 1 } fixed={false} minDelay={ 50 } maxDelay={ 200 }>
+                                        <h1 className="card-title" id="gamedev">G<br/>A<br/>M<br/>E<br/>D<br/>E<br/>V<br/></h1>
+                                    </TypeWriter>
+                                </div>  
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </section>    
         );
